bdda744a15311f6e192ebf0265360aa1531544c8
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-13 12:01:46 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "0e931694-e023-46f2-b2d3-63d0c066f5ad",
+    "home_feed": [
+        {
+            "id": "33c8883c-accc-4e7f-854a-4001fb7dbc54",
+            "artist_name": "artist",
+            "home_carousel": [
+                {
+                    "id": "5ee93310-5f7d-422e-965d-653d9ca16644",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/94.png"
+                },
+                {
+                    "id": "fc9b7053-4aea-4ad8-8bb2-53db0e79ab2e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/209.png"
+                },
+                {
+                    "id": "0799edaf-bdec-4b55-ac08-1254b88016a6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/83.png"
+                },
+                {
+                    "id": "ae6c2c64-83d2-41ac-8784-c838eea5ab9e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/49.png"
+                },
+                {
+                    "id": "432beff9-b722-42db-8c97-7e4fefe553ed",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/361.png"
+                }
+            ],
+            "art_caption": "energy",
+            "art_description": "Identify company west rule not manage. Meet effort care cause various require. Fact street idea parent between magazine receive."
+        },
+        {
+            "id": "cc6f899b-d2ea-42f2-97a4-21396d4d96f5",
+            "artist_name": "image",
+            "home_carousel": [
+                {
+                    "id": "ee116c54-d82f-4e4b-a7eb-64dcf5f0070d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/95.png"
+                },
+                {
+                    "id": "2fbf7dfc-752f-4a34-8433-d94557c2a9c4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/168.png"
+                },
+                {
+                    "id": "453900f4-b8ea-4049-9e49-ba0aa6b45704",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/277.png"
+                },
+                {
+                    "id": "fff98914-1b92-428e-86e4-b9d08d93c663",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/211.png"
+                },
+                {
+                    "id": "4e70438e-c9c1-44d8-b48e-af4b6ea2d4b0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/317.png"
+                }
+            ],
+            "art_caption": "sister",
+            "art_description": "Performance more near firm. Visit operation and huge black. High enter player take system rest."
+        },
+        {
+            "id": "6bcf1541-41af-4c62-9657-d66719fb49ad",
+            "artist_name": "management",
+            "home_carousel": [
+                {
+                    "id": "310efd79-457a-482a-93d7-07a2c5ff5e1a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/153.png"
+                },
+                {
+                    "id": "563cdaf7-5f1e-4b4d-9da6-14368c734618",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/183.png"
+                },
+                {
+                    "id": "8f7459f0-05f3-4861-9189-bde8f6d0dcf9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/273.png"
+                },
+                {
+                    "id": "1c4f8d58-ba4a-4d57-b068-11eff2ebc15c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/132.png"
+                },
+                {
+                    "id": "e32cf1a6-e4c8-4ae4-809b-fb5e7debf4e3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/356.png"
+                }
+            ],
+            "art_caption": "television",
+            "art_description": "Back government affect foreign seem. Attorney southern strategy own front item."
+        },
+        {
+            "id": "61fe1313-c157-485f-8a8d-ac3d76865423",
+            "artist_name": "leave",
+            "home_carousel": [
+                {
+                    "id": "81cc29f9-1ad5-41af-855d-e9129fc01ca9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/30.png"
+                },
+                {
+                    "id": "9e027d95-2658-4938-90e2-89d76409d1a6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/66.png"
+                },
+                {
+                    "id": "2429fba4-4f1e-4897-be0f-195c92f363d0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/18.png"
+                },
+                {
+                    "id": "8be68ff6-392a-4f1c-9d86-953251397e0e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/260.png"
+                },
+                {
+                    "id": "1e5dacf9-2d10-4a76-a012-a979c0f68c8b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/62.png"
+                }
+            ],
+            "art_caption": "already",
+            "art_description": "Bar hour matter kid. To put outside organization summer. Agent job group Democrat. Form represent want."
+        },
+        {
+            "id": "de63f3d0-dec0-42be-921d-757aec3b9657",
+            "artist_name": "mean",
+            "home_carousel": [
+                {
+                    "id": "4b04cb03-e2b4-4639-99c3-a58b6b926058",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/85.png"
+                },
+                {
+                    "id": "d732ffc9-b36e-4ea2-ad2b-e655721137af",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/304.png"
+                },
+                {
+                    "id": "8f2feb38-89f6-4210-ac4c-8b2dc14d1409",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/333.png"
+                },
+                {
+                    "id": "66d2bfac-33e9-4e81-a44c-41b7a1e96269",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/13.png"
+                },
+                {
+                    "id": "7c45c5e7-2d84-4e94-a677-1cc062ae7967",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/291.png"
+                }
+            ],
+            "art_caption": "also",
+            "art_description": "Actually after career lose author system across. News already what part image society left always. Live region factor available every move."
+        }
+    ]
+}
